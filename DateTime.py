d659c4cf4ffe1b6bddbedcc06953a9ff106adc5d
Olly <olivermarchington@live.co.uk>
2020-02-21 12:58:21 +0000

    started making the layout, finished location, weather and clock but need to implement them in the layout - olly

--- DateTime.py
+++ DateTime.py
@@ -1,3 +1,17 @@
 #''' This is the date and time script, bock will pull from this '''
 
 import datetime
+
+class Date_Time():
+
+    def get_date_time(self):
+        time_now = datetime.datetime.now().time().strftime('%H:%M')  # hour in 24h format
+        today = datetime.date.today()
+        date = today.strftime('%B %d, %Y')
+        day_of_week = today.strftime('%A')
+        date_dict = {
+            'time': time_now,
+            'date': date,
+            'day': day_of_week
+        }
+        return date_dict
