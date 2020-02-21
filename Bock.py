e3e195eac72f0d41ad528c4cdbd3cc5b26c585ba
Olly <olivermarchington@live.co.uk>
2020-02-21 11:59:03 +0000

    weather and location, if you require parts of location then edit as you want - olly

--- Bock.py
+++ Bock.py
@@ -1,1 +1,19 @@
-''' This will be the main script to run, it will call the other scripts from here and do the layout'''
+#''' This will be the main script to run, it will call the other scripts from here and do the layout'''
+#Import packages
+import tkinter
+import os
+
+#Import scripts
+import Weather, DateTime, Location
+
+# Classes
+location = Location.Location()
+weather = Weather.Weather()
+
+# tries to get the location, if it can't it'll return None
+lat_long = location.location()
+
+
+
+weather_currently = weather.get_weather(lat_long[0],lat_long[1])
+print(weather_currently)
--- Weather.py
+++ Weather.py
@@ -1,1 +1,26 @@
-''' This is the weather script, bock will pull this '''
+#''' This is the weather script, bock will pull this '''
+
+import pyowm
+import logging
+
+class Weather():
+
+    def get_weather(self,latitude,longitude):
+        try:
+            owm = pyowm.OWM('6892fc37f53067a561bbac518117974d')
+            observation = owm.weather_at_coords(latitude, longitude)
+            w = observation.get_weather()
+            temp = w.get_temperature('celsius')  # {'temp_max': 10.5, 'temp': 9.7, 'temp_min': 9.0}
+            temp = temp['temp']
+            degree_sign = u'\N{DEGREE SIGN}'
+            temperature = "{0},{1}".format(temp, degree_sign)
+            current_weather = w.get_detailed_status()
+            print('weather')
+        except Exception as e:
+            logging.error(e)
+            temp = 'unknown'
+            current_weather = 'unknown'
+            temperature = 'unknown'
+
+        dict = {'tempurature': temperature, "current_weather": current_weather, "temp_no_symbol": temp}
+        return dict
